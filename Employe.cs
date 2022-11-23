940306f720f583b86c2fc173c70ed638b3017edb
Woopser <petittom222@gmail.com>
2022-11-23 09:20:33 -0500

    Creation des classes

---
+++ Employe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_prets
+{
+    class Employe
+    {
+        string matricule;
+        string nom;
+        string prenom;
+
+        public string  Matricule { get => matricule; set => matricule = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+
+    }
+}
---
+++ Projet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_prets
+{
+    class Projet
+    {
+        string numero;
+        string debut;
+        int budget;
+        string descrip;
+        string employe;
+
+        public string Numero { get => numero; set => numero = value; }
+        public string Debut { get =>debut ; set => debut = value; }
+        public int Budget { get => budget; set => budget = value; }
+        public string Descrip { get => descrip; set => descrip = value; }
+        public string Employe { get => employe; set => employe = value; }
+
+
+
+    }
+}
